31053d78bc08afe6cd10ca5b1a86ed09b46cda84
Flavia Tani <98674255+flaviatani@users.noreply.github.com>
2023-01-31 08:44:19 -0300

    Criação da conexão com o banco de dados

---
+++ src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author flavi
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:musql://localhost:3306/ToDoTasks";
+    public static final String USER = "root";
+    public static final String PASS = "";
+
+    
+}
