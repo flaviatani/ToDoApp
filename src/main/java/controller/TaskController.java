8f957168deefb5e75fe556840c57e618b59c547a
Flavia Tani <98674255+flaviatani@users.noreply.github.com>
2023-02-01 00:31:51 -0300

    Salva as tarefas no banco de dados

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -4,6 +4,7 @@ package controller;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Date;
 import java.util.List;
 import model.Task;
 import util.ConnectionFactory;
@@ -12,6 +13,39 @@ import util.ConnectionFactory;
 public class TaskController {
     
     public void save(Task task){
+        
+        String sql = "INSERT INTO tasks (idProject"
+                + "name,"
+                + "description,"
+                + "completed,"
+                + "notes,"
+                + "deadline,"
+                + "createdAt,"
+                + "updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+         try {
+            
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,task.getIdProject());
+            statement.setString(2,task.getName());
+            statement.setString(3,task.getDescription());
+            statement.setBoolean(4,task.isIsCompleted());
+            statement.setString(5,task.getNotes());
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
+            statement.setBoolean(7, task.isCompleted());
+            statement.setDate(8, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(9, new Date(task.getUpdatedAt().getTime()));
+            
+            
+        }catch (SQLException e) {
+            throw new SQLException("Erro ao deletar a tarefa");
+        }finally {
+            ConnectionFactory.closeConnection(conn);
+        }
         
     }
     
