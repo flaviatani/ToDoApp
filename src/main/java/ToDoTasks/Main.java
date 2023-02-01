204da93924c0fe7f02a8793c45be805289ee58b5
Flavia Tani <98674255+flaviatani@users.noreply.github.com>
2023-01-31 22:38:49 -0300

    Conexão com o banco de dados

--- src/main/java/ToDoTasks/Main.java
+++ src/main/java/ToDoTasks/Main.java
@@ -5,17 +5,17 @@
  */
 package ToDoTasks;
 
-/**
- *
- * @author flavi
- */
+import java.sql.Connection;
+import util.ConnectionFactory;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Connection c = ConnectionFactory.getConnection();
+        
+        ConnectionFactory.closeConnection(c);
+        
     }
     
 }
--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -5,16 +5,33 @@
  */
 package util;
 
-/**
- *
- * @author flavi
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class ConnectionFactory {
     
     public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String URL = "jdbc:musql://localhost:3306/ToDoTasks";
+    public static final String URL = "jdbc:musql://localhost:3306/todoApp";
     public static final String USER = "root";
     public static final String PASS = "";
 
+    public static java.sql.Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conex�o com o banco de dados", ex);
+        }
+    }
     
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conex�o com o banco de dados", ex);
+        }
+    }    
 }
